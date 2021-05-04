c084eeb9686f441d993eb2bf945d0c01f0f17de8
Matt McMahon <contact@matt-mcmahon.com>
2021-05-03 23:49:56 -0400

    Add 34. Adding basic CSS styling

--- code/034-adding-basic-css-styling/src/components/ExpenseItem.tsx
+++ code/034-adding-basic-css-styling/src/components/ExpenseItem.tsx
@@ -1,15 +1,14 @@
+import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+export default function ExpenseItem() {
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>March 20, 2021</div>
       <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <div className="expense-item__price">$294.00</div>
       </div>
     </div>
   );
 }
-
-export default ExpenseItem;
