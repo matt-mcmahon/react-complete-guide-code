9124dd55e187f1e97f779008a7da73ac897c08a6
Matt McMahon <contact@matt-mcmahon.com>
2021-05-03 23:31:58 -0400

    Add ExpenseItem Component
    - Temporarily using in App to demonstrate

--- code/032-our-first-custom-component/src/App.tsx
+++ code/032-our-first-custom-component/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
