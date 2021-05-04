6106f3a33563bf089603dff5f5b5357555b3e7d9
Matt McMahon <contact@matt-mcmahon.com>
2021-05-03 23:21:25 -0400

    Add 32. Our First Custom Component

--- code/032-our-first-custom-component/src/App.tsx
+++ code/032-our-first-custom-component/src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
